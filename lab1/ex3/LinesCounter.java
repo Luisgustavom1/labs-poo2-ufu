96ff890ba417510b5e8c73d989355fef8fa5c168
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-03-11 14:43:49 -0300

    feat: ex3 with 10 files

---
+++ lab1/ex3/LinesCounter.java
@@ -0,0 +1,33 @@
+import java.io.LineNumberReader;
+import java.io.IOException;
+
+public class LinesCounter implements Runnable {
+    private LineNumberReader lineReader;
+    private int linesNumber = 0;
+
+    public LinesCounter (
+        LineNumberReader lineReader
+    ) {
+        this.lineReader = lineReader;
+    }
+
+    public void run() {
+        String line = "";
+
+        while(line != null) {
+            try {
+                line = this.lineReader.readLine();
+
+                if (line != null) {    
+                    this.linesNumber++;
+                }
+            } catch(IOException error) {
+                System.out.println(error.getMessage());
+            }
+        }
+    }
+
+    public int getLinesNumber() {
+        return this.linesNumber;
+    }
+}
---
+++ lab1/ex3/Main.java
@@ -0,0 +1,81 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList; 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String args[]) {
+        linesCounterWithMetrics(false);
+        linesCounterWithMetrics(true);
+    }
+
+    public static void linesCounterWithMetrics(boolean withThread) {
+        Path rootDir = Paths.get(".."); 
+        File[] files = new File(rootDir.toAbsolutePath() + "/files").listFiles();
+
+        try {
+            long startTime = System.currentTimeMillis();
+            System.out.println("\nTotal number of lines without thread -> " + ( 
+                withThread 
+                    ? linesCounterWithThread(files, rootDir) 
+                    : linesCounterWithoutThread(files, rootDir)
+                ));
+            long endTime = System.currentTimeMillis();
+            System.out.println("Time to read the files " + (endTime - startTime) + " milliseconds");
+        } catch (IOException error) {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    // Média de 96 milisegundos -> 10 arquivos 
+    public static int linesCounterWithoutThread(File[] files, Path rootDir) throws IOException {
+        int allLinesNumber = 0;
+
+        for(File file:files) {
+            FileReader fileReader = new FileReader(new File(rootDir.toAbsolutePath() + "/files/" + file.getName()));
+            LinesCounter linesCounter = new LinesCounter(new LineNumberReader(fileReader));
+
+            linesCounter.run();
+            allLinesNumber += linesCounter.getLinesNumber();
+        }
+
+        return allLinesNumber;
+    }
+
+    // Média de 19,33 -> 10 arquivos 
+    public static int linesCounterWithThread(File[] files, Path rootDir) throws IOException {
+        ThreadPoolExecutor poolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(files.length);
+        ArrayList<LinesCounter> linesCounters = new ArrayList<LinesCounter>();
+
+        for(File file:files) {
+            FileReader fileReader = new FileReader(new File(rootDir.toAbsolutePath() + "/files/" + file.getName()));
+            LinesCounter linesCounter = new LinesCounter(new LineNumberReader(fileReader));
+
+            linesCounters.add(linesCounter);
+            poolExecutor.execute(linesCounter);
+        }
+
+        poolExecutor.shutdown();
+        try {
+            poolExecutor.awaitTermination(50, TimeUnit.SECONDS);
+        } catch (InterruptedException error) {
+            System.out.println(error.getMessage());
+        }
+
+        int allLinesNumber = linesCounters
+            .stream()
+            .map(LinesCounter::getLinesNumber)
+            .reduce(
+                0,
+                (acc, current) -> acc + current
+            );
+
+        return allLinesNumber;
+    }
+}
