6b81d2f29e28275d7b1b4535567315717f890354
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-04-23 19:35:53 -0300

    feat: bridge

---
+++ design-patterns/structural/bridge/CRUD.java
@@ -0,0 +1,6 @@
+public interface CRUD {
+    void create(Object o);
+    Object read(Object o);
+    void update(Object o);
+    void delete(Object o);
+}
---
+++ design-patterns/structural/bridge/Register.java
@@ -0,0 +1,10 @@
+public class Register implements CRUD {
+    public Register() {}
+
+    public void create(Object o) {};
+    public Object read(Object o) {
+        return o;
+    };
+    public void update(Object o) {};
+    public void delete(Object o) {};
+}
